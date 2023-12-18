72c30836a4694a742c9f08a0d9bdfb9961bdf021
Yakup <yakupjoraev@gmail.com>
2023-12-18 20:15:48 +0300

    added modal form

--- src/js/main.js
+++ src/js/main.js
@@ -155,3 +155,87 @@ map();
 
 
 AOS.init();
+
+const openModalBtns = document.querySelectorAll('.open-modal-btn');
+const closeModalBtns = document.querySelectorAll('.close-modal-btn');
+const modals = document.querySelectorAll('.modal');
+
+openModalBtns.forEach(btn => {
+  btn.addEventListener('click', () => {
+    const modalId = btn.dataset.modalId;
+    const modal = document.getElementById(modalId);
+    modal.classList.add('show');
+  });
+});
+
+closeModalBtns.forEach(btn => {
+  btn.addEventListener('click', () => {
+    const modal = btn.closest('.modal');
+    modal.classList.remove('show');
+  });
+});
+
+window.addEventListener('click', (event) => {
+  if (event.target.classList.contains('modal')) {
+    event.target.classList.remove('show');
+  }
+});
+
+// validate////////////////////////////////////////////////////////////////////////////////////////////////////////
+let form = document.querySelector('.modal-form'),
+  formInputs = document.querySelectorAll('.js-input'),
+  inputEmail = document.querySelector('.js-input-email')
+// inputPhone = document.querySelector('.js-input-phone');
+
+
+function validateEmail(email) {
+  let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
+
+// function validatePhone(phone) {
+//   let re = /^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/;
+//   return re.test(String(phone));
+// }
+
+
+form.onsubmit = function () {
+  let emailVal = inputEmail.value,
+    // phoneVal = inputPhone.value,
+    emptyInputs = Array.from(formInputs).filter(input => input.value === '');
+
+  formInputs.forEach(function (input) {
+    if (input.value === '') {
+      input.classList.add('error');
+      console.log('input not filled');
+    } else {
+      input.classList.remove('error');
+    }
+
+
+  })
+
+  if (emptyInputs.length !== 0) {
+    console.log('inputs not filled');
+    return false;
+  }
+
+  if (!validateEmail(emailVal)) {
+    console.log('email not valid');
+    inputEmail.classList.add('error');
+    return false;
+  } else {
+    inputEmail.classList.remove('error');
+  }
+
+  // if (!validatePhone(phoneVal)) {
+  //   console.log('phone not valid');
+  //   inputPhone.classList.add('error');
+  //   return false;
+  // } else {
+  //   inputPhone.classList.remove('error');
+  // }
+
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
